5fabdc7387702a94d7707412fa480e642c63e0ee
AlexAlbino1 <144177349+AlexAlbino1@users.noreply.github.com>
2023-09-19 17:48:34 -0300

    Add files via upload

---
+++ ConsoleApp1/Editora.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Editora
+    {
+        private string nome;
+        private string site;
+        private string telephone;
+        static void Main(string[] args)
+        {
+            
+            Editora edit = new Editora();
+            edit.nome = ("nome do autor");
+            edit.site = ("site do autor");
+            edit.telephone = ("telefone do autor");
+
+            
+
+        }
+        private void exibirDados()
+        {
+            Console.WriteLine("editora" + nome);
+
+            Livro livro = new Livro();
+            livro.autor = ("nome do autor");
+            livro.valor = (-100);
+            livro.resumo = ("resumo do livro");
+            livro.paginas = (99);
+            livro.resumo = ("resumo do livro");
+            livro.nome = ("nome do livro");
+
+            livro.exibirDados();
+            Console.WriteLine(exibirDados);
+            
+            
+        }
+    }
+}
---
+++ ConsoleApp1/Livro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Livro
+    {
+        public String nome;
+        public String resumo;
+        public Double valor;
+        public int paginas;
+        public String autor;
+        
+
+        public void exibirDados()
+        {
+            string titulo = "detalhes do livro";
+            Console.WriteLine(titulo);
+            Console.WriteLine("nome" + nome);
+            Console.WriteLine("resumo" + resumo);
+            Console.WriteLine("valor" + valor);
+            Console.WriteLine("paginas" +  paginas);
+            Console.WriteLine("autor" +  autor);
+
+            
+        }
+    }
+}
